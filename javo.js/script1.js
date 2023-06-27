40e3da6b338d04320e399e9da8be8b2f5fdbfa29
Patricio Figueredo49 <131621260+Patric49@users.noreply.github.com>
2023-06-27 18:19:26 -0300

    Add files via upload

---
+++ javo.js/script1.js
@@ -0,0 +1,30 @@
+<script>
+
+  alert('Olá amigo (a) gostando do conteúdo?');
+  confirm('Está gostando de aprender um pouco mais sobre renda extra?');
+  let nome=prompt('Qual é o seu nome?');
+  alert('É um grande prazer em te conhecer, ' + nome + '!');
+  
+
+</script>
+
+
+ comentários
+  // instalação do node.js: novo terminal - node -v - instalações -g global  node nomedoarquivo //
+  //  npm é um pacote de ferramentas para ajudar no processamento de dados //
+  // VAR ou LET para iniciar uma variável/CONST é uma variável de valor constante
+  //(number + number) para adição e (string + string) para concatenação-junção //
+  //document.write - o que me retorna escrito // <br />
+  //MATH para variáveis numéricas - to fixed - .replace( '.',',')
+  //n1.tolocalestring('pt-br',{style:'currency', currency: 'BRL'}) valores convertidos dinheiro
+  //S.lenght( )
+  //S.to uppercase( )
+  //S.to lowercase( ) - formatar strings ( " " ' ' ` ` `` ´´)
+  //window.alert('a soma dos valores é' +string ou +number(S)); Caixa de diálogo
+  //prompt( '' );  return(  );  console.log = ""
+  //Array = variável composta com cada uma tendo uma chave (kay) ou um índice  
+  
+
+
+
+
